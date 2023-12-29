c622895f4cb7487025f2de3050ad137ff64be7ad
Jingcheng Wang <jingwang49@gmail.com>
2023-12-29 22:11:15 +1100

    login and logout functions

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import ProductList from './components/ProductList'
 
 import Context from './Context'
 
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
+
 export default class App extends Component {
   constructor (props) {
     super(props)
@@ -18,6 +21,43 @@ export default class App extends Component {
     }
     this.routerRef = React.createRef()
   }
+
+  componentDidMount() {
+    let user = localStorage.getItem("user");
+    user = user ? JSON.parse(user) : null;
+    this.setState({ user });
+  }
+
+  login = async (email, password) => {
+    const res = await axios.post(
+      'http://localhost:3001/login',
+      { email, password },
+    ).catch((res) => {
+      return { status: 401, message: 'Unauthorized' }
+    })
+  
+    if(res.status === 200) {
+      const { email } = jwtDecode(res.data.accessToken)
+      const user = {
+        email,
+        token: res.data.accessToken,
+        accessLevel: email === 'admin@example.com' ? 0 : 1
+      }
+  
+      this.setState({ user });
+      localStorage.setItem("user", JSON.stringify(user));
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  logout = e => {
+    e.preventDefault();
+    this.setState({ user: null });
+    localStorage.removeItem("user");
+  };
+
   render () {
     return (
       <Context.Provider
