e66f775eef47d6146e3d3d2ad8a70f3a785be1b2
Jingcheng Wang <jingwang49@gmail.com>
2023-12-30 22:57:16 +1100

    fix link to bulma.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bulma/css/bulma.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +16,15 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//Terminal input, before npm start
+//Set-ExecutionPolicy -Scope Process Bypass
+//./node_modules/.bin/json-server-auth ./backend/db.json --port 3001
+
+//Access level 1
+//regular@example.com
+//password
+
+//Access level 0
+//admin@example.com
+//password
